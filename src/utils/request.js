e9958957de52709f7ef5852ec2c8759f61b38854
wuchuanchuan <superwucc@gmail.com>
2023-09-18 17:58:55 +0800

    接口超时处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -9,7 +9,7 @@ import { baseApi } from '@/utils/env'
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 60000 // request timeout
+  timeout: 30000 // request timeout
 })
 
 // request interceptor
